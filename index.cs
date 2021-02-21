29110fbf453b1aecc5afa653b1e49731dad508a5
Istiakshovon <58106197+Istiakshovon@users.noreply.github.com>
2021-02-21 10:53:04 +0600

    Create index.cs

---
+++ index.cs
@@ -0,0 +1,66 @@
+//GetComponent(file)
+GetComponent<Rigidbody2D>();
+
+//Change velocity
+rb.velocity = new Vector2(velocityX, velocityY);
+
+//scale(size) of object
+Vector3 playerScale = snake.localScale;
+
+//Follow player movement
+public Transform player;
+public Vector3 offset;
+
+// Update is called once per frame
+void Update()
+{
+    Debug.Log(player.position);
+    transform.position = player.position + offset;
+}
+
+//Add Force
+rb.AddForce(30 * Time.deltaTime,0,0, ForceMode.VelocityChange);
+
+//Get UI build index
+SceneManager.GetActiveScene().buildIndex
+
+//LoadScene
+SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+//visibility of *GameObject*
+completeLevelUI.SetActive(true);
+
+//Time
+float time = Time.time;
+
+//GetTouch
+var touch = Input.GetTouch(0);
+
+//TouchPosition
+var touchPos = touch.position;
+
+//isRight of touching
+var isRight = touchPos.x >= Screen.width / 2f;
+
+//GetObject(file) and call class
+FindObjectOfType<GameManager>().EndGame();
+
+//QuitApplication
+Application.Quit();
+
+//Check Collision with object
+void OnCollisionEnter(Collision collisionInfo)
+{
+    Debug.Log(collisionInfo.collider.name);
+    if (collisionInfo.collider.tag == "obstacle")
+    {
+        movement.enabled = false;
+        FindObjectOfType<GameManager>().EndGame();
+    }else if(collisionInfo.collider.name == "END"){
+        FindObjectOfType<GameManager>().CompleteLevel();
+    }
+}
+
+//GetPosition of object of specific axis
+Text scoreText.text = player.position.z.ToString("0");
+
